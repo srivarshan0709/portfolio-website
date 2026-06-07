b908300f6ae6d05d165ab30acd248860769d014f
SRIVARSHAN S <srivarshan668@gmail.com>
2026-06-07 17:18:52 +0530

    Delete contact.js

--- contact.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { sendContactMessage } from '../controllers/contactController.js';
-
-const router = Router();
-
-router.post('/', sendContactMessage);
-
-export default router;
